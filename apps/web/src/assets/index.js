dee37c07d3eb45a2b1d09e098ba2f1c6b8ce0598
CosLynx AI <drixisgod@gmail.com>
2024-11-08 00:27:28 +0530

    generated file: `apps/web/src/assets/index.js`

---
+++ apps/web/src/assets/index.js
@@ -0,0 +1,57 @@
+import logo from './images/logo.png';
+import profilePlaceholder from './images/profilePlaceholder.jpg';
+import workoutIcon from './images/workoutIcon.svg';
+
+import CheckIcon from './icons/CheckIcon.jsx';
+import ArrowDownIcon from './icons/ArrowDownIcon.jsx';
+import CloseIcon from './icons/CloseIcon.jsx';
+import EditIcon from './icons/EditIcon.jsx';
+import DeleteIcon from './icons/DeleteIcon.jsx';
+import AddIcon from './icons/AddIcon.jsx';
+import StarIcon from './icons/StarIcon.jsx';
+import InfoIcon from './icons/InfoIcon.jsx';
+import SettingsIcon from './icons/SettingsIcon.jsx';
+import CalendarIcon from './icons/CalendarIcon.jsx';
+import UserIcon from './icons/UserIcon.jsx';
+import HomeIcon from './icons/HomeIcon.jsx';
+import LogoutIcon from './icons/LogoutIcon.jsx';
+import SearchIcon from './icons/SearchIcon.jsx';
+import FilterIcon from './icons/FilterIcon.jsx';
+import SortIcon from './icons/SortIcon.jsx';
+import ChartIcon from './icons/ChartIcon.jsx';
+import ProfileIcon from './icons/ProfileIcon.jsx';
+import GraphIcon from './icons/GraphIcon.jsx';
+import ListIcon from './icons/ListIcon.jsx';
+import SettingsOutlinedIcon from './icons/SettingsOutlinedIcon.jsx';
+import ShareIcon from './icons/ShareIcon.jsx';
+
+import FontProvider from './fonts/index.js';
+
+export {
+  logo,
+  profilePlaceholder,
+  workoutIcon,
+  CheckIcon,
+  ArrowDownIcon,
+  CloseIcon,
+  EditIcon,
+  DeleteIcon,
+  AddIcon,
+  StarIcon,
+  InfoIcon,
+  SettingsIcon,
+  CalendarIcon,
+  UserIcon,
+  HomeIcon,
+  LogoutIcon,
+  SearchIcon,
+  FilterIcon,
+  SortIcon,
+  ChartIcon,
+  ProfileIcon,
+  GraphIcon,
+  ListIcon,
+  SettingsOutlinedIcon,
+  ShareIcon,
+  FontProvider,
+};
