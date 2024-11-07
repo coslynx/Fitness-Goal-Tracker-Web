f657ac8f6cea8b524ad2fc05d1c75c726fd68567
CosLynx AI <drixisgod@gmail.com>
2024-11-08 00:38:13 +0530

    generated file: `apps/web/src/components/index.js`

---
+++ apps/web/src/components/index.js
@@ -0,0 +1,87 @@
+// apps/web/src/components/index.js
+
+// Import all shared components
+import Button from './shared/Button.jsx';
+import Input from './shared/Input.jsx';
+import Modal from './shared/Modal.jsx';
+import Select from './shared/Select.jsx';
+import Checkbox from './shared/Checkbox.jsx';
+import Radio from './shared/Radio.jsx';
+import TextField from './shared/TextField.jsx';
+import Typography from './shared/Typography.jsx';
+import Avatar from './shared/Avatar.jsx';
+import IconButton from './shared/IconButton.jsx';
+import Card from './shared/Card.jsx';
+import Grid from './shared/Grid.jsx';
+import List from './shared/List.jsx';
+import Table from './shared/Table.jsx';
+import Tooltip from './shared/Tooltip.jsx';
+import Snackbar from './shared/Snackbar.jsx';
+import Dialog from './shared/Dialog.jsx';
+import Tabs from './shared/Tabs.jsx';
+import Accordion from './shared/Accordion.jsx';
+import Drawer from './shared/Drawer.jsx';
+import AppBar from './shared/AppBar.jsx';
+import Toolbar from './shared/Toolbar.jsx';
+import Menu from './shared/Menu.jsx';
+import MenuItem from './shared/MenuItem.jsx';
+import Divider from './shared/Divider.jsx';
+import Breadcrumbs from './shared/Breadcrumbs.jsx';
+import Chip from './shared/Chip.jsx';
+import Badge from './shared/Badge.jsx';
+import Progress from './shared/Progress.jsx';
+import Skeleton from './shared/Skeleton.jsx';
+import CircularProgress from './shared/CircularProgress.jsx';
+import LinearProgress from './shared/LinearProgress.jsx';
+import Alert from './shared/Alert.jsx';
+import Paper from './shared/Paper.jsx';
+import Container from './shared/Container.jsx';
+import Box from './shared/Box.jsx';
+import Flex from './shared/Flex.jsx';
+import Stack from './shared/Stack.jsx';
+import SvgIcon from './shared/SvgIcon.jsx';
+import Icon from './shared/Icon.jsx';
+
+// Re-export all components
+export { 
+  Button,
+  Input,
+  Modal,
+  Select,
+  Checkbox,
+  Radio,
+  TextField,
+  Typography,
+  Avatar,
+  IconButton,
+  Card,
+  Grid,
+  List,
+  Table,
+  Tooltip,
+  Snackbar,
+  Dialog,
+  Tabs,
+  Accordion,
+  Drawer,
+  AppBar,
+  Toolbar,
+  Menu,
+  MenuItem,
+  Divider,
+  Breadcrumbs,
+  Chip,
+  Badge,
+  Progress,
+  Skeleton,
+  CircularProgress,
+  LinearProgress,
+  Alert,
+  Paper,
+  Container,
+  Box,
+  Flex,
+  Stack,
+  SvgIcon,
+  Icon
+};
