fe46037b7af96a80c68df28abb38b67595e5e2d3
CosLynx AI <drixisgod@gmail.com>
2024-11-08 00:26:52 +0530

    generated file: `apps/web/src/assets/images/index.js`

---
+++ apps/web/src/assets/images/index.js
@@ -0,0 +1,5 @@
+import logo from './logo.png';
+import profilePlaceholder from './profilePlaceholder.jpg';
+import workoutIcon from './workoutIcon.svg';
+
+export { logo, profilePlaceholder, workoutIcon };
